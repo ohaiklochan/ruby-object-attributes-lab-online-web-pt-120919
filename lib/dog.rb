e9c79efe043e563b7a50713d7d1e76ed8c5410bb
Chloe Foronda <caforonda103@yahoo.com>
2020-01-12 22:55:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,3 +2,13 @@ class Dog
   
   def name(dog_name)
     @this_dogs_name=(dog_name)
+  end
+  
+  def breed(dog_breed)
+    @breed=(dog_breed)
+  end
+  
+  wishbone = Dog.new
+  wishbone.name = "Wishbone"
+  
+  
